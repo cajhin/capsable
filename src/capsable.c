7b90aca150317e85ee739f9d47f208fbc8f18732
Joern Hinrichsen <cajhin@posteo.net>
2022-06-05 04:12:48 +0200

    v4 alt special chars complete

--- src/capsable.c
+++ src/capsable.c
@@ -3,8 +3,59 @@
 #include <linux/input.h>
 #include <unistd.h>
 
-#define DEBUG if(1)  
-#define VERSION 2
+#define DEBUG if(0)  
+#define VERSION 4
+
+
+const struct input_event sync_event = {.type = EV_SYN, .code = SYN_REPORT,   .value = 0};
+struct input_event event, modEvent;
+int modmask = 0;
+
+void setModdedKey(int _modmask, unsigned short key)
+{
+    modmask = _modmask;
+    event.code = key;
+}
+
+void produceModdedKey()
+{
+        modEvent.type = EV_KEY;
+
+        if (modmask & 0b0001) {
+            DEBUG fprintf(stderr, "modmask lshf");
+            modEvent.code = KEY_LEFTSHIFT;
+        }
+        if (modmask & 0b0010) {
+            DEBUG fprintf(stderr, "modmask lctrl");
+            modEvent.code = KEY_LEFTCTRL;
+        }
+
+        modEvent.value = 1;
+        modEvent.time.tv_sec = event.time.tv_sec;
+        modEvent.time.tv_usec = event.time.tv_usec - 1;
+        if (event.time.tv_usec == 0)
+            modEvent.time.tv_sec--;
+        fwrite(&modEvent, sizeof(modEvent), 1, stdout);
+
+        event.value = 1;
+        fwrite(&event, sizeof(event), 1, stdout);
+
+        event.value = 0;
+        event.time.tv_usec++;
+        fwrite(&event, sizeof(event), 1, stdout);
+
+        fwrite(&sync_event, sizeof(sync_event), 1, stdout);
+        usleep(25000);
+
+        modEvent.value = 0;
+        modEvent.time.tv_sec = event.time.tv_sec;
+        modEvent.time.tv_usec = event.time.tv_usec + 1;
+        if (modEvent.time.tv_usec == 0)
+            modEvent.time.tv_sec+=1;
+        fwrite(&modEvent, sizeof(modEvent), 1, stdout);
+}
+
+
 
 int main(void) 
 {
@@ -13,29 +64,23 @@ int main(void)
 
     setbuf(stdin, NULL), setbuf(stdout, NULL);
 
-    const struct input_event 
-        sync_event = {.type = EV_SYN, .code = SYN_REPORT,   .value = 0};
-
-    struct input_event event, modEvent;
-
     int capsIsDown = 0;
     int escIsDown = 0;
     int altIsDown = 0;
-    unsigned int modmask = 0;
 
     while (fread(&event, sizeof(event), 1, stdin) == 1) 
     {
         modmask = 0;
 
-        if(event.type != EV_KEY)
-        {
+        if(event.type != EV_KEY) {
             fwrite(&event, sizeof(event), 1, stdout);
             continue;
         }
 
-        if (event.code == KEY_F2) {  //todo ESC on baremetal F2 in VM
+        if (event.code == KEY_ESC ||
+            event.code == KEY_F1) {
             escIsDown = event.value == 0 ? 0 : 1;
-            DEBUG fprintf(stderr, "F2 escIsDown:%u\n", escIsDown);
+            DEBUG fprintf(stderr, "ESC/F1 escIsDown:%u\n", escIsDown);
             fwrite(&event, sizeof(event), 1, stdout);
             continue;
         }
@@ -88,109 +133,93 @@ int main(void)
                 event.code = KEY_HOME;
             else if (event.code == KEY_U)
                 event.code = KEY_END;
-            else if (event.code == KEY_N) {
-                if(!event.value)
-                    continue;
-                modmask = 0b0010;
-                event.code = KEY_LEFT;
-            }
-            else if (event.code == KEY_M) {
-                if(!event.value)
-                    continue;
-                modmask = 0b0010;
-                event.code = KEY_RIGHT;
-            }
-            else if (event.code == KEY_A) {
-                if(!event.value)
-                    continue;
-                modmask = 0b0010;
-                event.code = KEY_Z;
-            }
-            else if (event.code == KEY_S) {
-                if(!event.value)
-                    continue;
-                modmask = 0b0010;
-                event.code = KEY_X;
-            }
-            else if (event.code == KEY_D) {
-                if(!event.value)
-                    continue;
-                modmask = 0b0010;
-                event.code = KEY_C;
-            }
-            else if (event.code == KEY_F) {
-                if(!event.value)
-                    continue;
-                modmask = 0b0010;
-                event.code = KEY_V;
-            }
+            else if (event.code == KEY_N) 
+                setModdedKey(2,KEY_LEFT);
+            else if (event.code == KEY_M) 
+                setModdedKey(2,KEY_RIGHT);
+            else if (event.code == KEY_A) 
+                setModdedKey(2, KEY_Z);
+            else if (event.code == KEY_S)
+                setModdedKey(2, KEY_X);
+            else if (event.code == KEY_D)
+                setModdedKey(2, KEY_C);
+            else if (event.code == KEY_F)
+                setModdedKey(2, KEY_V);
         }
 
         //alt chars
         if(altIsDown)
         {
-            if (event.code == KEY_F)
-                event.code = KEY_SLASH;
-            else if (event.code == KEY_Q) {
-                if(!event.value)
-                    continue;
-                modmask = 0b0001;
-                event.code = KEY_1;
-            }
+                 if (event.code == KEY_Q) 
+                setModdedKey(1, KEY_1);
+            else if (event.code == KEY_W) 
+                setModdedKey(1, KEY_2);
+            else if (event.code == KEY_E) 
+                setModdedKey(1, KEY_3);
+            else if (event.code == KEY_R) 
+                setModdedKey(1, KEY_4);
+            else if (event.code == KEY_T) 
+                setModdedKey(1, KEY_5);
+            else if (event.code == KEY_Z) //TODO read original event
+                setModdedKey(1, KEY_6);
+            else if (event.code == KEY_U) 
+                setModdedKey(1, KEY_7);
+            else if (event.code == KEY_I) 
+                setModdedKey(1, KEY_8);
+            else if (event.code == KEY_O) 
+                setModdedKey(1, KEY_9);
+            else if (event.code == KEY_P) 
+                setModdedKey(1, KEY_0);
+            else if (event.code == KEY_A) 
+                setModdedKey(0, KEY_MINUS);
+            else if (event.code == KEY_S) 
+                setModdedKey(1, KEY_EQUAL);
+            else if (event.code == KEY_D) 
+                setModdedKey(1, KEY_8);
+            else if (event.code == KEY_F) 
+                setModdedKey(0, KEY_SLASH);
+            else if (event.code == KEY_G) 
+                setModdedKey(0, KEY_EQUAL);
+            else if (event.code == KEY_H) 
+                setModdedKey(0, KEY_BACKSPACE);
+            // else if (event.code == KEY_J) 
+            //     setModdedKey(1, KEY_);
+            else if (event.code == KEY_K) 
+                setModdedKey(1, KEY_LEFTBRACE);
+            else if (event.code == KEY_L) 
+                setModdedKey(1, KEY_RIGHTBRACE);
+            else if (event.code == KEY_Y) //TODO read orig event not rewired
+                setModdedKey(0, KEY_GRAVE);
+            else if (event.code == KEY_X) 
+                setModdedKey(1, KEY_GRAVE);
+            else if (event.code == KEY_C) 
+                setModdedKey(1, KEY_BACKSLASH);
+            else if (event.code == KEY_V) 
+                setModdedKey(1, KEY_MINUS);
+            // else if (event.code == KEY_) 
+            //     setModdedKey(1, KEY_);
+            else if (event.code == KEY_N) 
+                setModdedKey(1, KEY_COMMA);
+            else if (event.code == KEY_M) 
+                setModdedKey(1, KEY_DOT);
+            else if (event.code == KEY_COMMA) 
+                setModdedKey(0, KEY_LEFTBRACE);
+            else if (event.code == KEY_DOT) 
+                setModdedKey(0, KEY_RIGHTBRACE);
+            // else if (event.code == KEY_) 
+            //     setModdedKey(1, KEY_);
         }
 
-        //try out with numbers
-        else if (event.code == KEY_1)  {
-            if(capsIsDown)
-                event.code = KEY_A;
-        }
-        else
-            DEBUG fprintf(stderr, "other code: %u - %i\n", event.code, event.value);
+        DEBUG fprintf(stderr, "code: %u - %i\n", event.code, event.value);
 
-
-
+        //write
         if (modmask)
         {
-            modEvent.type = EV_KEY;
-
-            if (modmask & 0b0001) {
-                DEBUG fprintf(stderr, "modmask lshf");
-                modEvent.code = KEY_LEFTSHIFT;
-            }
-            if (modmask & 0b0010) {
-                DEBUG fprintf(stderr, "modmask lctrl");
-                modEvent.code = KEY_LEFTCTRL;
-            }
-            
-
-            modEvent.value = 1;
-            modEvent.time.tv_sec = event.time.tv_sec;
-            modEvent.time.tv_usec = event.time.tv_usec - 1;
-            if (event.time.tv_usec == 0)
-                modEvent.time.tv_sec--;
-            fwrite(&modEvent, sizeof(modEvent), 1, stdout);
-
-            event.value = 1;
-            fwrite(&event, sizeof(event), 1, stdout);
-
-            event.value = 0;
-            event.time.tv_usec++;
-            fwrite(&event, sizeof(event), 1, stdout);
-
-            fwrite(&sync_event, sizeof(sync_event), 1, stdout);
-            usleep(25000);
-
-            modEvent.value = 0;
-            modEvent.time.tv_sec = event.time.tv_sec;
-            modEvent.time.tv_usec = event.time.tv_usec + 1;
-            if (modEvent.time.tv_usec == 0)
-                modEvent.time.tv_sec+=1;
-            fwrite(&modEvent, sizeof(modEvent), 1, stdout);
-
+            if(event.value)  //whole combo is written on key down
+                produceModdedKey(modmask);
         }
         else
             fwrite(&event, sizeof(event), 1, stdout);
     }
-
 }
 
