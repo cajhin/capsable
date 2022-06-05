bdc0deb8f2b2dafc21a1e6ac29e6a91ba225ff4c
Joern Hinrichsen <cajhin@posteo.net>
2022-06-05 15:00:57 +0200

    v5 tuning away various issues

--- src/capsable.c
+++ src/capsable.c
@@ -3,56 +3,77 @@
 #include <linux/input.h>
 #include <unistd.h>
 
-#define DEBUG if(0)  
-#define VERSION 4
+#define DEBUG if(1)  
+#define VERSION 5
 
+//fuzzy. VSCode needs no sleep, gnome apps a lot
+#define SYN_SLEEP_US 10000
 
-const struct input_event sync_event = {.type = EV_SYN, .code = SYN_REPORT,   .value = 0};
+const struct input_event syn_report = {.type = EV_SYN, .code = SYN_REPORT,   .value = 0};
 struct input_event event, modEvent;
-int modmask = 0;
+int key_handled = 0;
 
-void setModdedKey(int _modmask, unsigned short key)
+void writeSynReport()
 {
-    modmask = _modmask;
-    event.code = key;
+    //syn_report.time
+    fwrite(&syn_report, sizeof(syn_report), 1, stdout);
+    usleep(SYN_SLEEP_US);
 }
 
-void produceModdedKey()
+void writeKey(unsigned short code)
+{ 
+    DEBUG fprintf(stderr, "KEY out: %u - %i\n", event.code, event.value);
+    key_handled = 1;
+    event.code = code;
+    fwrite(&event, sizeof(event), 1, stdout);
+}
+
+void writeModdedKey(int modmask, unsigned short code)
 {
-        modEvent.type = EV_KEY;
+    if(!modmask)
+    {
+        writeKey(code);
+        return;
+    }
 
-        if (modmask & 0b0001) {
-            DEBUG fprintf(stderr, "modmask lshf");
-            modEvent.code = KEY_LEFTSHIFT;
-        }
-        if (modmask & 0b0010) {
-            DEBUG fprintf(stderr, "modmask lctrl");
-            modEvent.code = KEY_LEFTCTRL;
-        }
+    key_handled = 1;
+    if(!event.value) //entire sequence is written on key down
+        return;
 
-        modEvent.value = 1;
-        modEvent.time.tv_sec = event.time.tv_sec;
-        modEvent.time.tv_usec = event.time.tv_usec - 1;
-        if (event.time.tv_usec == 0)
-            modEvent.time.tv_sec--;
-        fwrite(&modEvent, sizeof(modEvent), 1, stdout);
+    event.code = code;
 
-        event.value = 1;
-        fwrite(&event, sizeof(event), 1, stdout);
+    modEvent.type = EV_KEY;
+    if (modmask & 0b0001) {
+        DEBUG fprintf(stderr, "modmask lshf");
+        modEvent.code = KEY_LEFTSHIFT;
+    }
+    if (modmask & 0b0010) {
+        DEBUG fprintf(stderr, "modmask lctrl");
+        modEvent.code = KEY_LEFTCTRL;
+    }
 
-        event.value = 0;
-        event.time.tv_usec++;
-        fwrite(&event, sizeof(event), 1, stdout);
+    modEvent.value = 1;
+    modEvent.time.tv_sec = event.time.tv_sec;
+    modEvent.time.tv_usec = event.time.tv_usec - 1;
+    if (event.time.tv_usec == 0)
+        modEvent.time.tv_sec--;
+    fwrite(&modEvent, sizeof(modEvent), 1, stdout);
+    writeSynReport();
 
-        fwrite(&sync_event, sizeof(sync_event), 1, stdout);
-        usleep(25000);
+    event.value = 1;
+    writeKey(event.code);
 
-        modEvent.value = 0;
-        modEvent.time.tv_sec = event.time.tv_sec;
-        modEvent.time.tv_usec = event.time.tv_usec + 1;
-        if (modEvent.time.tv_usec == 0)
-            modEvent.time.tv_sec+=1;
-        fwrite(&modEvent, sizeof(modEvent), 1, stdout);
+    event.value = 0;
+    event.time.tv_usec++;
+    writeKey(event.code);
+
+    modEvent.value = 0;
+    modEvent.time.tv_sec = event.time.tv_sec;
+    modEvent.time.tv_usec = event.time.tv_usec + 1;
+    if (modEvent.time.tv_usec == 0)
+        modEvent.time.tv_sec+=1;
+    fwrite(&modEvent, sizeof(modEvent), 1, stdout);
+    writeSynReport();
 }
 
 
@@ -64,24 +85,35 @@ int main(void)
 
     setbuf(stdin, NULL), setbuf(stdout, NULL);
 
-    int capsIsDown = 0;
     int escIsDown = 0;
+    int capsIsDown = 0;
     int altIsDown = 0;
 
     while (fread(&event, sizeof(event), 1, stdin) == 1) 
     {
-        modmask = 0;
+        key_handled = 0;
 
-        if(event.type != EV_KEY) {
+        if(event.type == EV_MSC && event.code == MSC_SCAN)
+            continue;
+
+        if(event.type == EV_SYN) {
+            DEBUG fprintf(stderr," SYN: %i\n",event.code);
             fwrite(&event, sizeof(event), 1, stdout);
             continue;
         }
 
+        if(event.type != EV_KEY) {
+            DEBUG fprintf(stderr,"UNEXPECTED EV TYPE: %i\n",event.type);
+            fwrite(&event, sizeof(event), 1, stdout);
+            continue;
+        }
+
+        //ESC, MODIFIER
         if (event.code == KEY_ESC ||
             event.code == KEY_F1) {
             escIsDown = event.value == 0 ? 0 : 1;
             DEBUG fprintf(stderr, "ESC/F1 escIsDown:%u\n", escIsDown);
-            fwrite(&event, sizeof(event), 1, stdout);
+            writeKey(event.code);
             continue;
         }
         else if (escIsDown && event.code == KEY_X)  {
@@ -103,6 +135,8 @@ int main(void)
             event.code = KEY_RIGHTALT;
         else if (event.code == KEY_SLASH)
             event.code = KEY_RIGHTSHIFT;
+        else if (event.code == KEY_BACKSLASH)
+            event.code = KEY_ENTER;
         else if (event.code == KEY_102ND) //ISO boards left <>
             event.code = KEY_LEFTSHIFT;
         else if (event.code == KEY_Y)
@@ -114,112 +148,105 @@ int main(void)
         if(capsIsDown)
         {
             if (event.code == KEY_J)
-                event.code = KEY_LEFT;
+                writeKey(KEY_LEFT);
             else if (event.code == KEY_L)
-                event.code = KEY_RIGHT;
+                writeKey(KEY_RIGHT);
             else if (event.code == KEY_I)
-                event.code = KEY_UP;
+                writeKey(KEY_UP);
             else if (event.code == KEY_K)
-                event.code = KEY_DOWN;
+                writeKey(KEY_DOWN);
             else if (event.code == KEY_O)
-                event.code = KEY_PAGEUP;
+                writeKey(KEY_PAGEUP);
             else if (event.code == KEY_DOT)
-                event.code = KEY_PAGEDOWN;
+                writeKey(KEY_PAGEDOWN);
             else if (event.code == KEY_H)
-                event.code = KEY_BACKSPACE;
+                writeKey(KEY_BACKSPACE);
             else if (event.code == KEY_SEMICOLON)
-                event.code = KEY_DELETE;
+                writeKey(KEY_DELETE);
             else if (event.code == KEY_Z)
-                event.code = KEY_HOME;
+                writeKey(KEY_HOME);
             else if (event.code == KEY_U)
-                event.code = KEY_END;
+                writeKey(KEY_END);
             else if (event.code == KEY_N) 
-                setModdedKey(2,KEY_LEFT);
+                writeModdedKey(2,KEY_LEFT);
             else if (event.code == KEY_M) 
-                setModdedKey(2,KEY_RIGHT);
+                writeModdedKey(2,KEY_RIGHT);
+            else if (event.code == KEY_COMMA) 
+                writeModdedKey(1,KEY_RIGHT);
+            
             else if (event.code == KEY_A) 
-                setModdedKey(2, KEY_Z);
+                writeModdedKey(2, KEY_Z);
             else if (event.code == KEY_S)
-                setModdedKey(2, KEY_X);
+                writeModdedKey(2, KEY_X);
             else if (event.code == KEY_D)
-                setModdedKey(2, KEY_C);
+                writeModdedKey(2, KEY_C);
             else if (event.code == KEY_F)
-                setModdedKey(2, KEY_V);
+                writeModdedKey(2, KEY_V);
         }
-
-        //alt chars
-        if(altIsDown)
+        //ALT CHARS !@#$%^&()
+        else if(altIsDown)
         {
                  if (event.code == KEY_Q) 
-                setModdedKey(1, KEY_1);
+                writeModdedKey(1, KEY_1);
             else if (event.code == KEY_W) 
-                setModdedKey(1, KEY_2);
+                writeModdedKey(1, KEY_2);
             else if (event.code == KEY_E) 
-                setModdedKey(1, KEY_3);
+                writeModdedKey(1, KEY_3);
             else if (event.code == KEY_R) 
-                setModdedKey(1, KEY_4);
+                writeModdedKey(1, KEY_4);
             else if (event.code == KEY_T) 
-                setModdedKey(1, KEY_5);
+                writeModdedKey(1, KEY_5);
             else if (event.code == KEY_Z) //TODO read original event
-                setModdedKey(1, KEY_6);
+                writeModdedKey(1, KEY_6);
             else if (event.code == KEY_U) 
-                setModdedKey(1, KEY_7);
+                writeModdedKey(1, KEY_7);
             else if (event.code == KEY_I) 
-                setModdedKey(1, KEY_8);
+                writeModdedKey(1, KEY_9);
             else if (event.code == KEY_O) 
-                setModdedKey(1, KEY_9);
+                writeModdedKey(1, KEY_0);
             else if (event.code == KEY_P) 
-                setModdedKey(1, KEY_0);
+                writeModdedKey(1, KEY_SLASH);
             else if (event.code == KEY_A) 
-                setModdedKey(0, KEY_MINUS);
+                writeModdedKey(0, KEY_MINUS);
             else if (event.code == KEY_S) 
-                setModdedKey(1, KEY_EQUAL);
+                writeModdedKey(1, KEY_EQUAL);
             else if (event.code == KEY_D) 
-                setModdedKey(1, KEY_8);
+                writeModdedKey(1, KEY_8);
             else if (event.code == KEY_F) 
-                setModdedKey(0, KEY_SLASH);
+                writeModdedKey(0, KEY_SLASH);
             else if (event.code == KEY_G) 
-                setModdedKey(0, KEY_EQUAL);
+                writeModdedKey(0, KEY_EQUAL);
             else if (event.code == KEY_H) 
-                setModdedKey(0, KEY_BACKSPACE);
-            // else if (event.code == KEY_J) 
-            //     setModdedKey(1, KEY_);
+                writeModdedKey(0, KEY_BACKSPACE);
+            else if (event.code == KEY_J) 
+                 writeModdedKey(0, KEY_BACKSLASH);
             else if (event.code == KEY_K) 
-                setModdedKey(1, KEY_LEFTBRACE);
+                writeModdedKey(1, KEY_LEFTBRACE);
             else if (event.code == KEY_L) 
-                setModdedKey(1, KEY_RIGHTBRACE);
+                writeModdedKey(1, KEY_RIGHTBRACE);
             else if (event.code == KEY_Y) //TODO read orig event not rewired
-                setModdedKey(0, KEY_GRAVE);
+                writeModdedKey(0, KEY_GRAVE);
             else if (event.code == KEY_X) 
-                setModdedKey(1, KEY_GRAVE);
+                writeModdedKey(1, KEY_GRAVE);
             else if (event.code == KEY_C) 
-                setModdedKey(1, KEY_BACKSLASH);
+                writeModdedKey(1, KEY_BACKSLASH);
             else if (event.code == KEY_V) 
-                setModdedKey(1, KEY_MINUS);
-            // else if (event.code == KEY_) 
-            //     setModdedKey(1, KEY_);
+                writeModdedKey(1, KEY_MINUS);
+            else if (event.code == KEY_B) 
+                 continue;
             else if (event.code == KEY_N) 
-                setModdedKey(1, KEY_COMMA);
+                writeModdedKey(1, KEY_COMMA);
             else if (event.code == KEY_M) 
-                setModdedKey(1, KEY_DOT);
+                writeModdedKey(1, KEY_DOT);
             else if (event.code == KEY_COMMA) 
-                setModdedKey(0, KEY_LEFTBRACE);
+                writeModdedKey(0, KEY_LEFTBRACE);
             else if (event.code == KEY_DOT) 
-                setModdedKey(0, KEY_RIGHTBRACE);
-            // else if (event.code == KEY_) 
-            //     setModdedKey(1, KEY_);
+                writeModdedKey(0, KEY_RIGHTBRACE);
         }
-
-        DEBUG fprintf(stderr, "code: %u - %i\n", event.code, event.value);
 
         //write
-        if (modmask)
-        {
-            if(event.value)  //whole combo is written on key down
-                produceModdedKey(modmask);
-        }
-        else
-            fwrite(&event, sizeof(event), 1, stdout);
+        if (!key_handled)
+            writeKey(event.code);
     }
 }
 
--- start_caps.sh
+++ start_caps.sh
@@ -8,4 +8,7 @@ INTERDEV=/dev/input/by-path/platform-i8042-serio-0-event-kbd
 BUILDDIR=/home/jjj/git/capsable/build
 sleep 1
 echo capsable go...
-sudo $BUILDDIR/intercept -g $INTERDEV | $BUILDDIR/capsable | $BUILDDIR/uinput_$DISTRO -d $INTERDEV
+
+sudo nice -n -20 $BUILDDIR/intercept -g $INTERDEV | nice -n -20 $BUILDDIR/capsable | nice -n -20 $BUILDDIR/uinput_$DISTRO -d $INTERDEV
+#sudo $BUILDDIR/intercept -g $INTERDEV | $BUILDDIR/capsable | $BUILDDIR/uinput_$DISTRO -d $INTERDEV
+
