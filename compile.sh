36113950b26da390543abb8e10a6467fdff5a306
Joern Hinrichsen <cajhin@posteo.net>
2026-05-07 15:16:56 +0200

    clean up build/deploy scripts

--- compile.sh
+++ compile.sh
@@ -3,3 +3,8 @@ set -e
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 mkdir -p "$SCRIPT_DIR/build"
 clang "$SCRIPT_DIR/src/capsable-core.c" -o "$SCRIPT_DIR/build/capsable-core"
+for bin in intercept uinput; do
+    if [ ! -f "$SCRIPT_DIR/build/$bin" ]; then
+        cp "$SCRIPT_DIR/dist/bin/$bin" "$SCRIPT_DIR/build/$bin"
+    fi
+done
